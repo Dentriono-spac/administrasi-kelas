05af3c11a59f707c263c7df9083c191ddccf691a
Dentriono-spac <triono52@guru.sd.belajar.id>
2026-09-11 08:33:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+```javascript
 // ======================================
 // ADMINISTRASI KELAS
 // SCRIPT UTAMA
@@ -16,7 +17,7 @@ function bukaAbsensi() {
 // Membuka aplikasi Perpustakaan
 function bukaPerpustakaan() {
   window.open(
-    "https://dentriono-spac.github.io/perpustakaan-sekolah/";
+    "https://dentriono-spac.github.io/perpustakaan-sekolah/",
     "_blank"
   );
 }
@@ -24,19 +25,19 @@ function bukaPerpustakaan() {
 
 // Membuka Portofolio TRIONO, S.Pd.
 function bukaPortofolio() {
-    window.open(
-    "https://sites.google.com/guru.sd.belajar.id/dentriono/";
+  window.open(
+    "https://sites.google.com/guru.sd.belajar.id/dentriono/",
     "_blank"
   );
 }
 
+
 // Menu yang belum dibuat
 function menuBelumTersedia(namaMenu) {
-
   alert(
     "Menu " + namaMenu +
     " sedang dalam tahap pengembangan.\n\n" +
     "Nanti akan kita sambungkan ke sistem Administrasi Kelas."
   );
-
 }
+```
