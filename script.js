2346963dff44d70002b2e6f0397e8219a9a6d69d
Dentriono-spac <triono52@guru.sd.belajar.id>
2026-09-11 07:34:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// ======================================
+// ADMINISTRASI KELAS
+// SCRIPT UTAMA
+// ======================================
+
+
+// Membuka aplikasi Absensi QR
+function bukaAbsensi() {
+
+  window.location.href =
+    "https://dentriono-spac.github.io/absensi-qr-sekolah/";
+
+}
+
+
+// Membuka aplikasi Perpustakaan
+function bukaPerpustakaan() {
+
+  window.location.href =
+    "https://dentriono-spac.github.io/perpustakaan-sekolah/";
+
+}
+
+
+// Menu yang belum dibuat
+function menuBelumTersedia(namaMenu) {
+
+  alert(
+    "Menu " + namaMenu +
+    " sedang dalam tahap pengembangan.\n\n" +
+    "Nanti akan kita sambungkan ke sistem Administrasi Kelas."
+  );
+
+}
